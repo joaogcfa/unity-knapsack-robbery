ffd9fa4ece7d4f1e90b532eeab65f790f5fd55fd
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2022-06-06 19:29:59 -0300

    animacoes players

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,30 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10.0f;
     public float inputX;
     public float inputY;
+    public Animator animation;
+
+
+    float x, y;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float inputX;
         float inputY;
+
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+
+
         if (P1)
         {
             inputX = Input.GetAxis("P1Horizontal");
@@ -33,11 +45,56 @@ public class PlayerMovement : MonoBehaviour
             inputY = Input.GetAxis("P2Vertical");
         }
 
+        if (inputY < 0)
+        {
+            animation.SetBool("isWalking", true);
+            animation.SetBool("isBack", false);
+
+
+        }
+        else if (inputY > 0)
+        {
+            animation.SetBool("isWalking", false);
+            animation.SetBool("isBack", true);
+
+
+        }
+        else
+        {
+
+            animation.SetBool("isWalking", false);
+            animation.SetBool("isBack", false);
+
+        }
+
+
+        if (inputX < 0)
+        {
+            animation.SetBool("isLeft", true);
+            animation.SetBool("isRight", false);
+
+
+        }
+        else if (inputX > 0)
+        {
+            animation.SetBool("isLeft", false);
+            animation.SetBool("isRight", true);
+
+
+        }
+        else
+        {
+
+            animation.SetBool("isLeft", false);
+            animation.SetBool("isRight", false);
+
+        }
+
 
         Vector3 desiredVelocity = inputX * transform.right + inputY * transform.forward;
 
         velocidade = new Vector2(inputX * speed, inputY * speed);
         rb.MovePosition(rb.position + velocidade * Time.fixedDeltaTime);
-        
+
     }
 }
