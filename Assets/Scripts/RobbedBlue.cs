e0a29383dd3dbb052e1a262eeb7b0ea962549e4c
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2022-06-08 18:18:57 -0300

    objetos pulsando

--- Assets/Scripts/RobbedBlue.cs
+++ Assets/Scripts/RobbedBlue.cs
@@ -10,7 +10,7 @@ public class RobbedBlue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.Find("Player2");
     }
 
     // Update is called once per frame
--- Assets/Scripts/RobbedOrange.cs
+++ Assets/Scripts/RobbedOrange.cs
@@ -10,6 +10,7 @@ public class RobbedOrange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player1");
 
     }
 
